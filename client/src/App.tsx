e09b6b126be9096e2a05fa58e7297bda1ceef9ca
Rohan Singh <rohan.connect.now@gmail.com>
2026-03-04 20:59:04 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/:rest*" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
